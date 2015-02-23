39d86b0d2652529491274029f14128d6deb48541
Andrew Godfroy <andrew.godfroy@killerrin.com>
2015-02-23 15:50:23 -0500

    Added Sort Extension for ObservableCollection

--- Killerrin Studios Toolkit.Shared/KTKExtensions.cs
+++ Killerrin Studios Toolkit.Shared/KTKExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Text;
 
@@ -42,6 +44,26 @@ namespace KillerrinStudiosToolkit
                 newText.Append(text[i]);
             }
             return newText.ToString();
+        }
+
+        public static void Sort<T>(this ObservableCollection<T> observable) where T : IComparable<T>, IEquatable<T>
+        {
+            List<T> sorted = observable.OrderBy(x => x).ToList();
+
+            int ptr = 0;
+            while (ptr < sorted.Count)
+            {
+                if (!observable[ptr].Equals(sorted[ptr]))
+                {
+                    T t = observable[ptr];
+                    observable.RemoveAt(ptr);
+                    observable.Insert(sorted.IndexOf(t), t);
+                }
+                else
+                {
+                    ptr++;
+                }
+            }
         }
     }
 }
